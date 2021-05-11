69142f6e22d2b38ae0608bf38f4fdb3d5a8b4855
Shin Jun Cheol <904sinjun@gmail.com>
2021-05-11 19:57:01 +0900

    초기 커밋.

---
+++ baekjun/Hello.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// 곱셈 3, 4, 5, 6 출력
+int main(void) {
+	int a;
+	string b;
+	vector<int> vec;
+
+	cin >> a >> b;
+
+	for (int i = b.length() - 1; i >= 0; --i)
+	{
+		int c = b[i] - '0';
+		vec.push_back(a * c);
+	}
+
+	vec.push_back(a * stoi(b));
+
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		printf("%d\n", vec[i]);
+	}
+
+
+	return 0;
+}
